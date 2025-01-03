03ef8d7dd3f633b411074294c0ee6777cdde96c7
Roberto Torres Martínez <roberxiri@gmail.com>
2025-01-03 08:24:08 +0000

    remove import Board

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,6 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./Navbar";
-import Board from "./Board";
 
 //create your first component
 const Home = () => {
