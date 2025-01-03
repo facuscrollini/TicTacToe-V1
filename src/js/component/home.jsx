0e81690d7c32451abb829c3ccd86385cbd6d4a3a
Roberto Torres Martínez <roberxiri@gmail.com>
2025-01-03 08:17:47 +0000

    New Board

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,7 +11,21 @@ const Home = () => {
 
 	return (
 		<>
-			<Board />
+			<Navbar />
+			<div className="text-center">
+				<h1 className="text-center mt-5">Hello Rigo!</h1>
+				<p>
+					<img src={rigoImage} />
+				</p>
+				<a href="#" className="btn btn-success">
+					If you see this green button... bootstrap is working...
+				</a>
+				<p>
+					Made by{" "}
+					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+					love!
+				</p>
+			</div>
 		</>
 	);
 };
