da95556e787b4eb0ea371e663378db318a1567d9
ALCIDESCONTRERAS <alcidescon@gmail.com>
2025-01-03 07:31:14 +0000

    Fichas listas

---
+++ src/js/component/Fichas.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Fichas = ()=>{
+    return (
+        <>
+            <p className="fichas Ficha-X">X</p>
+            <p className="fichas Ficha-O">O</p>
+        </>
+    )
+}
+
+export default Fichas
